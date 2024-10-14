1bc8d8d1a6e5d8cfa47e448ad2e0e1aecc6ad166
Zachary Siegel <zachary@siegel.com>
2024-10-14 21:23:15 +0000

    added reproduction command for o1

--- reproduce_results.sh
+++ reproduce_results.sh
@@ -1,49 +1,53 @@
 # This script contains the commands to reproduce all experiments in the CORE-Bench paper. In total, ten different experiments were ran. The commands can be ran concurrently, although you might hit Azure rate limit errors.
 
 # ------- Experiments on Train Set -------
-# 1) AutoGPT with GPT-4o
+# AutoGPT with GPT-4o
 python3 main.py --experiment_name "train_autogpt_gpt4o_c-4" --benchmark_level "codeocean_easy" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o.sh" --platform azure
 python3 main.py --experiment_name "train_autogpt_gpt4o_c-4" --benchmark_level "codeocean_medium" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o.sh" --platform azure
 python3 main.py --experiment_name "train_autogpt_gpt4o_c-4" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o.sh" --platform azure
 
-# 2) AutoGPT with GPT-4o-mini
+# AutoGPT with GPT-4o-mini
 python3 main.py --experiment_name "train_autogpt_gpt4o-mini_c-4" --benchmark_level "codeocean_easy" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o-mini.sh" --platform azure
 python3 main.py --experiment_name "train_autogpt_gpt4o-mini_c-4" --benchmark_level "codeocean_medium" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o-mini.sh" --platform azure
 python3 main.py --experiment_name "train_autogpt_gpt4o-mini_c-4" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o-mini.sh" --platform azure
 
-# 3) CORE-Agent with GPT-4o
+# CORE-Agent with GPT-4o
 python3 main.py --experiment_name "train_coreagent_gpt4o_c-4" --benchmark_level "codeocean_easy" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_easy_gpt4o.sh" --platform azure
 python3 main.py --experiment_name "train_coreagent_gpt4o_c-4" --benchmark_level "codeocean_medium" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_medium_gpt4o.sh" --platform azure
 python3 main.py --experiment_name "train_coreagent_gpt4o_c-4" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_hard_gpt4o.sh" --platform azure
 
-# 4) CORE-Agent with GPT-4o-mini
+# CORE-Agent with GPT-4o-mini
 python3 main.py --experiment_name "train_coreagent_gpt4o-mini_c-4" --benchmark_level "codeocean_easy" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_easy_gpt4o-mini.sh" --platform azure
 python3 main.py --experiment_name "train_coreagent_gpt4o-mini_c-4" --benchmark_level "codeocean_medium" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_medium_gpt4o-mini.sh" --platform azure
 python3 main.py --experiment_name "train_coreagent_gpt4o-mini_c-4" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_hard_gpt4o-mini.sh" --platform azure
 
-# 5) CoreAgent with GPT-4o (Cost Ablation)
+# CoreAgent with GPT-4o (Cost Ablation)
 python3 main.py --experiment_name "train_coreagent_gpt4o_c-10" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_hard_gpt4o_cost_10.sh" --platform azure
 
-# 6) CoreAgent with GPT-4o-mini (Cost Ablation)
+# CoreAgent with GPT-4o-mini (Cost Ablation)
 python3 main.py --experiment_name "train_coreagent_gpt4o-mini_c-10" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_train.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_hard_gpt4o-mini_cost_10.sh" --platform azure
 
 # ------- Experiments on Test Set -------
-# 7) AutoGPT with GPT-4o
+# AutoGPT with GPT-4o
 python3 main.py --experiment_name "test_autogpt_gpt4o_c-4" --benchmark_level "codeocean_easy" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o.sh" --platform azure
 python3 main.py --experiment_name "test_autogpt_gpt4o_c-4" --benchmark_level "codeocean_medium" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o.sh" --platform azure
 python3 main.py --experiment_name "test_autogpt_gpt4o_c-4" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o.sh" --platform azure
 
-# 8) AutoGPT with GPT-4o-mini
+# AutoGPT with GPT-4o-mini
 python3 main.py --experiment_name "test_autogpt_gpt4o-mini_c-4" --benchmark_level "codeocean_easy" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o-mini.sh" --platform azure
 python3 main.py --experiment_name "test_autogpt_gpt4o-mini_c-4" --benchmark_level "codeocean_medium" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o-mini.sh" --platform azure
 python3 main.py --experiment_name "test_autogpt_gpt4o-mini_c-4" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "autogpt_gpt4o-mini.sh" --platform azure
 
-# 9) CORE-Agent with GPT-4o
+# CORE-Agent with GPT-4o
 python3 main.py --experiment_name "test_coreagent_gpt4o_c-4" --benchmark_level "codeocean_easy" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_easy_gpt4o.sh" --platform azure
 python3 main.py --experiment_name "test_coreagent_gpt4o_c-4" --benchmark_level "codeocean_medium" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_medium_gpt4o.sh" --platform azure
 python3 main.py --experiment_name "test_coreagent_gpt4o_c-4" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_hard_gpt4o.sh" --platform azure
 
-# 10) CORE-Agent with GPT-4o-mini
+# CORE-Agent with GPT-4o-mini
 python3 main.py --experiment_name "test_coreagent_gpt4o-mini_c-4" --benchmark_level "codeocean_easy" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_easy_gpt4o-mini.sh" --platform azure
 python3 main.py --experiment_name "test_coreagent_gpt4o-mini_c-4" --benchmark_level "codeocean_medium" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_medium_gpt4o-mini.sh" --platform azure
 python3 main.py --experiment_name "test_coreagent_gpt4o-mini_c-4" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_hard_gpt4o-mini.sh" --platform azure
+
+# ------- Experiments on Test Set (not included in paper) -------
+# CORE-Agent with o1-mini
+python3 main.py --experiment_name "test_coreagent_o1-mini_c-10" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_hard_o1-mini_cost_10.sh" --platform azure --resume benchmark/results/test_coreagent_o1-mini_c-10/20241013-200729_codeocean_hard.json
