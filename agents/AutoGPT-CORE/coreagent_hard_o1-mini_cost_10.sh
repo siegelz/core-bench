6888bd52f53ac7e7ddea5b6bf2a974b905d7604a
Zachary Siegel <zachary@siegel.com>
2024-10-25 00:28:46 +0000

    use gpt-4o for vlm in o1

--- agents/AutoGPT-CORE/coreagent_hard_o1-mini_cost_10.sh
+++ agents/AutoGPT-CORE/coreagent_hard_o1-mini_cost_10.sh
@@ -76,7 +76,7 @@ sudo ln -s $(pwd)/environment/$cap_subdir/results /results
   --skip-reprompt \
   --continuous \
   --log-level DEBUG \
-  --vlm "o1-mini-2024-09-12" --fast_llm "o1-mini-2024-09-12" --smart_llm "o1-mini-2024-09-12" --programmatic_key_check \
+  --vlm "gpt-4o-2024-05-13" --fast_llm "o1-mini-2024-09-12" --smart_llm "o1-mini-2024-09-12" --programmatic_key_check \
   --openai_cost_budget 10
 
 cd ..
