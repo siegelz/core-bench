eaed9564d306fc2b345dc1b16cf543e9154b2139
Zachary Siegel <zachary@siegel.com>
2024-10-25 03:25:20 +0000

    add cmd to reproduce claude results

--- reproduce_results.sh
+++ reproduce_results.sh
@@ -51,3 +51,6 @@ python3 main.py --experiment_name "test_coreagent_gpt4o-mini_c-4" --benchmark_le
 # ------- Experiments on Test Set (not included in paper) -------
 # CORE-Agent with o1-mini
 python3 main.py --experiment_name "test_coreagent_o1-mini_c-10" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_hard_o1-mini_cost_10.sh" --platform azure --resume benchmark/results/test_coreagent_o1-mini_c-10/20241013-200729_codeocean_hard.json
+
+# CORE-Agent with Claude-3-5-Sonnet
+python main.py --experiment_name "test_coreagent_claude_35_sonnet_c-4" --benchmark_level "codeocean_hard" --dataset_file "benchmark/dataset/core_test.json" --agent_dir "agents/AutoGPT-CORE" --agent_script "coreagent_hard_claude_35_sonnet.sh" --platform azure
